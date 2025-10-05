3cf308993fe0f9e26b18caefda706b4b055a2bbc
theus006 <matheuscostadeo@gmail.com>
2025-10-05 16:02:20 -0300

    atualizando arquivo de teste

--- config/database.sql
+++ config/database.sql
@@ -12,7 +12,7 @@ create table Sensors (
     id int primary key auto_increment,
     name varchar(30) not null,
     value decimal(5,2),
-    time time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     id_user int,
     foreign key (id_user) references Users(id)
 );
